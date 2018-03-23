fb722f04b476f170319897afd51d83a4579431ed
s.salcedo.2017 <sandra.salcedo.gostanza@gmail.com>
2018-03-23 16:33:26 +0100

    work

---
+++ openfda1/practice1.py
@@ -0,0 +1,33 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+
+print("The id is:", repos['results'][0]["id"])
+print("The purpose is:", repos['results'][0]["purpose"])
+print("The manofacture name is:", repos['results'][0]["openfda"]["manufacturer_name"])
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+i = 0
+while i < 10:
+    print("The id is", repos['results'][i]['id'])
+    i += 1
