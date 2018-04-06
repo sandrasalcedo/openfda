f2f2cd9b782d1522760be56698a40ba27b92705e
s.salcedo.2017 <sandra.salcedo.gostanza@gmail.com>
2018-04-06 16:11:25 +0200

    class

--- openfda-2/showrepos2.py
+++ openfda-2/showrepos2.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic&limit=7", None, headers)
+conn.request("GET", "/drug/label.json?search=active_ingredient:acetylsalicylic&limit=10", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
