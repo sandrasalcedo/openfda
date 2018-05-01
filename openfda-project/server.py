97aae8fa08b1e0514219e2932d046d2d24074af1
s.salcedo.2017 <sandra.salcedo.gostanza@gmail.com>
2018-05-01 19:05:41 +0200

    hgf

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -13,27 +13,42 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+
         if self.path == "/":
             with open("search1.html", "r") as f:
                 message = f.read()
             self.wfile.write(bytes(message, "utf8"))
 
-        elif "search" in self.path:
+        elif "searchdrug" in self.path:
             headers = {'User-Agent': 'http-client'}
             conn = http.client.HTTPSConnection("api.fda.gov")
             data = self.path.strip("search?").split("&")
             drug = data[0].split("=")[1]
             limit = data[1].split("=")[1]
             url = "/drug/label.json?search=active_ingredient:" + drug + "&" + "limit=" + limit
-            print(url)
             conn.request("GET", url, None, headers)
             r1 = conn.getresponse()
             drugs_raw = r1.read().decode("utf-8")
             conn.close()
             drugs = json.loads(drugs_raw)
-            new_drugs = str(drugs)
-            self.wfile.write(bytes(new_drugs, "utf8"))
-        return
+            list1 = []
+
+
+            for i in range(len(drugs['results'])):
+                list.append(drugs['results'][i]['active_ingredient'][0])
+            intro = "<!doctype html>" + "<html>" + "<body>" + "<ul>"
+            end = "</ul>" + "</body>" + "</html>"
+            with open("blank.html", 'w') as f:
+                f.write(intro)
+                for element in list:
+                f.write("<li>" + element + "</li>")
+                f.write(end)
+            with open('blank.html', 'r') as f:
+                 file = f.read()
+                 self.wfile.write(bytes(file, "utf8"))
+        elif "searchcompany" in self.path:
+
+
 
 
 Handler = testHTTPRequestHandler
